8cccb405c7ba357c985320a83ef68f679e539064
kmahk <127909366+kmahk@users.noreply.github.com>
2024-05-22 23:30:18 +0900

    테스트

---
+++ app/src/main/java/com/example/lsb/SplashActivity.java
@@ -0,0 +1,44 @@
+package com.example.lsb;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.lsb.MainActivity;
+import com.example.lsb.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        // 효과음 재생
+        mediaPlayer = MediaPlayer.create(this, R.raw.sb_300420172);
+        mediaPlayer.start();
+
+        // 5초 후에 메인 액티비티로 전환
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 5000);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // 액티비티가 종료될 때 미디어 플레이어 해제
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+    }
+}
